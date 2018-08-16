ece5b96c6b13aaa58f027a45033b1d2fb1124924
Ephraim Kunz <ephraimkunz@me.com>
2018-08-16 07:30:41 -0700

    Add anti-aliasing

---
+++ hittable_list.hpp
@@ -0,0 +1,28 @@
+#ifndef HITTABLELISTH
+#define HITTABLELISTH
+
+#include "hittable.hpp"
+
+class hittable_list: public hittable {
+public:
+    hittable **list;
+    int list_size;
+    hittable_list() {}
+    hittable_list(hittable **l, int n) {list = l; list_size = n;}
+    virtual bool hit(const ray& r, float tmin, float tmax, hit_record& rec) const {
+        hit_record temp_rec;
+        bool hit_anything = false;
+        double closest_so_far = tmax;
+        for (int i = 0; i < list_size; ++i) {
+            if (list[i]->hit(r, tmin, closest_so_far, temp_rec)) {
+                hit_anything = true;
+                closest_so_far = temp_rec.t;
+                rec = temp_rec;
+            }
+        }
+
+        return hit_anything;
+    }
+};
+
+#endif // HITTABLELISTH
--- main.cpp
+++ main.cpp
@@ -2,36 +2,52 @@
 #include <fstream>
 #include "vec3.hpp"
 #include "ray.hpp"
+#include "hittable_list.hpp"
+#include "sphere.hpp"
+#include "camera.hpp"
 
 using namespace std;
 
-vec3 color(const ray& r) {
-    vec3 unit_direction = unit_vector(r.direction()); // Scale to [-1, 1]
-    float t = 0.5*(unit_direction.y() + 1.0); // Move to [0, 1]
-    return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0); // Linear interpolation (white to blue)
+vec3 color(const ray& r, hittable *world) {
+    hit_record rec;
+    if (world->hit(r, 0.0, MAXFLOAT, rec)) {
+        return 0.5 * vec3(rec.normal.x()+1, rec.normal.y()+1, rec.normal.z()+1);
+    } else {
+        vec3 unit_direction = unit_vector(r.direction());
+        float t = 0.5*(unit_direction.y() + 1.0); // Move to [0, 1]
+        return (1.0 - t)*vec3(1.0, 1.0, 1.0) + t*vec3(0.5, 0.7, 1.0); // Linear interpolation (white to blue)
+    }   
 }
 
 int main() {
+    int nx = 200;
+    int ny = 100; 
+    int ns = 100;
+    
     ofstream file;
     file.open("out.ppm");
 
-    const int nx = 200;
-    const int ny = 100;
-
     file << "P3\n" << nx << " " << ny << "\n255\n";
 
-    // Image 2 x 4
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
+    camera cam;
+
+    hittable *list[2];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5);
+    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hittable *world = new hittable_list(list, 2);
 
     for(int j = ny - 1; j >= 0; j--) {
         for(int i = 0; i < nx; i++) {
-            float u = float(i) / float(nx);
-            float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u*horizontal + v*vertical);
-            vec3 col = color(r);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
+                float v = float(j + drand48()) / float(ny);
+                ray r = cam.get_ray(u, v);
+                vec3 p = r.point_at_parameter(2.0);
+                col  += color(r, world);
+            }
+            
+            col /= float(ns);
             int ir = int(255.99 * col.r());
             int ig = int(255.99 * col.g());
             int ib = int(255.99 * col.b());
